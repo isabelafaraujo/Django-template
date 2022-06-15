44035f5af04cd7f6c0f1315f8193ebd06a44cacf
isabelafaraujo <belaaraujobr@protonmail.com>
2022-06-15 03:12:52 -0300

    feat: route creation

--- api/library/urls.py
+++ api/library/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import  routers
+from api.registration.apiModules import views as registrationviewsets
+
+route = routers.DefaultRouter()
+route.register(r'registration/', registrationviewsets.RegistrationViewSet, basename="Registration")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',include(route.urls))
 ]
